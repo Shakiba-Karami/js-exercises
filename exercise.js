12cf9e4ad35578de8c9b3ed76e111fa856e159ff
shakiba-karami <Shakibakarami@yahoo.com>
2025-05-27 15:15:50 +0200

    sum by forEach & reduce

---
+++ exercise.js
@@ -0,0 +1,24 @@
+const company = {
+    name: "TechCorp",
+    departments: {
+        dev: {employees: 15, budget: 100000},
+        hr: {employees: 3, budget: 50000},
+    }
+};
+//a) Access the HR budget
+console.log(company.departments.hr.budget)
+//b) Add new department "marketing" with .employees = 5
+company.departments.marketing = {
+    employees: 5,
+}
+console.log(company.departments)
+//c)calculate total employees using Object.values
+let employees_count = 0
+Object.values(company.departments).forEach((dep)=> {
+    employees_count = dep.employees + employees_count
+})
+console.log(employees_count)
+
+let employees_count2 = Object.values(company.departments).reduce((acc, cur) => 
+    acc + cur.employees ,0)
+console.log(employees_count2)
