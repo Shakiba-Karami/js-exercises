196fa02f62500b41931156fc1c5e5a70c1b7f9d8
shakiba-karami <Shakibakarami@yahoo.com>
2025-05-31 01:25:32 +0200

    forEach, filter, map

--- exercise.js
+++ exercise.js
@@ -1,3 +1,124 @@
+///////////sum of divisible by 3
+const sum3 = (input) => {
+    let result = 0
+    for (let i=0 ; i <= input; i++ ) {
+    if (!(i%3)) { 
+        result += i 
+    }
+    }
+    return result
+}
+console.log (sum3(6))
+////////////////
+const people = [
+  { name: "Alice", age: 25, job: "Engineer" },
+  { name: "Bob", age: 32, job: "Designer" },
+  { name: "Charlie", age: 29, job: "Teacher" },
+  { name: "Diana", age: 24, job: "Developer" },
+  { name: "Ethan", age: 35, job: "Manager" },
+  { name: "Fiona", age: 28, job: "Engineer" },
+  { name: "George", age: 41, job: "CEO" },
+  { name: "Hannah", age: 30, job: "Marketing" },
+  { name: "Ian", age: 27, job: "Developer" },
+  { name: "Jasmine", age: 33, job: "HR" }
+];
+console.log(people[1].age)
+
+// log the names of each item & jobs of each
+
+for (let i = 0; i < people.length ; i++ ) {
+    console.log(people[i].name)
+}
+
+const name_log = () => {
+    let result =[]
+    people.forEach((item) => {
+        result.push(item.name)
+    })
+    return result
+}
+console.log(name_log())
+
+people.forEach((person) => {
+    console.log(person.name)
+})
+people.forEach((person_job) => {
+    console.log(person_job.job)
+})
+// make an array of the people whose job is engineer
+const engineers = []
+people.forEach((person) => {
+    if (person.job === 'Engineer') {
+        engineers.push(person)
+    }
+})
+console.log(engineers)
+// make an array of the people whose job is Developer
+const developers = people.filter ((person_job) => {
+    return person_job.job === "Developer"
+})
+console.log(developers)
+// Add a new key 'is old' with the values true (if age>30) or false (if age <=30)
+const old_or_not = people.map ((person) => {
+    if (person.age > 30) {
+        return  person.name + ' is old : true' 
+    }
+    return  person.name + ' is old : false' 
+})
+console.log(old_or_not)
+
+const p2 = people.map ((person) => {
+    const is_old = person.age > 30
+    return {
+        name: person.name,
+        is_old: is_old
+    }
+})
+console.log(p2)
+
+const p3 = people.map ((person) => {
+    return {
+        name: person.name,
+        age: person.age,
+        job: person.job,
+        is_old: person.age > 30
+    }
+})
+console.log(p3)
+//////////////////
+const numbers = [7, 14, 3, 14, 0, 19, 6, 20, 2, 8, 14, 3, 5, 11, 7, 18, 9, 6, 13, 1]
+
+const doubled = numbers.map((number) => {
+    return number * 2
+})
+console.log(doubled)
+
+const divisible_by2 = numbers.filter((number) => {
+    return !(number % 2)
+})
+console.log(divisible_by2)
+// sum of greater than 10
+let sum = 0
+const greaterThan10 = numbers.filter((number) => {
+       return number >= 10
+})
+console.log(greaterThan10)
+greaterThan10.forEach((number) => {
+    sum += number
+}
+)
+console.log(sum)
+
+let sumOfGreaterThan10 = 0
+numbers.forEach((number) => {
+    if (number >= 10) {
+        sumOfGreaterThan10 += number
+    }
+}
+)
+console.log(sumOfGreaterThan10)
+/////////////////
+
 const company = {
     name: "TechCorp",
     departments: {
@@ -23,7 +144,7 @@ let employees_count2 = Object.values(company.departments).reduce((acc, cur) =>
     acc + cur.employees ,0)
 console.log(employees_count2)
 
-///////////////
+///////////////array from object by entries and vice versa by fromEntries;
 const myArray = [
     ["name", "John"],
     ["lastname", "Doe"],
@@ -35,3 +156,20 @@ console.log(objFromArray)
 const arrFromobj = Object.entries(objFromArray)
 console.log(arrFromobj)
 /////////////////
+const people2 = {
+    ali: 23,
+    jay: 31,
+    reza: 32
+}
+let ageAverage2 = 0 
+const sum2 = Object.values(people2)
+console.log(sum2)
+ageAverage2 = sum2.reduce((acc, cur) => acc + cur, 0)/sum2.length
+console.log(ageAverage2)
+
+///length of a string
+console.log(objFromArray.name.length)
+/////////////
+
+
+
